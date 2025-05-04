6baa7234f96157357150d1bb193e80d22b9e8eac
Mohsen Deniku <mohsen_deniku@icloud.com>
2025-05-04 11:32:06 +0330

    pa

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="qr-code">
-        <img src="/public/images/image-qr-code.png" alt="QR Code" />
+        <img src="public/images/image-qr-code.png" alt="QR Code" />
         <h2>Improve your front-end skills by building projects</h2>
         <p>
           Scan the QR code to visit Frontend Mentor and take your coding skills
