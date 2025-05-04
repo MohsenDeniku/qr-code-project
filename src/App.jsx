64607ec703ffdfeeba97d80484bcfb00f02cd3a0
Mohsen Deniku <mohsen_deniku@icloud.com>
2025-05-04 11:24:53 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="qr-code">
+        <img src="/public/images/image-qr-code.png" alt="QR Code" />
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+}
